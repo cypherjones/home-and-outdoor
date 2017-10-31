c5438fd5156d63dfc7017db220fc775b00143ffc
AJ Jones <ajjonez@icloud.com>
2017-10-31 12:39:32 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+	// $('.moo-menu-item').each(function() {
+	//     if ($(this).find('img').length) {
+	//        console.log('yes')
+	//     }
+	// });
+	$('.moo-menu-list-item').addClass('foo'); 
+})
