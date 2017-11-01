4cc84544420a25bab61e46d5d50f1095aef90ec6
AJ Jones <ajjonez@icloud.com>
2017-10-31 20:41:29 -0500

    test add class to hero btn-->add css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
 jQuery(document).ready(function($){
-	// $('.moo-menu-item').each(function() {
-	//     if ($(this).find('img').length) {
-	//        console.log('yes')
-	//     }
-	// });
 	$('.hero-btn-size').css('width', '240px'); 
 })
